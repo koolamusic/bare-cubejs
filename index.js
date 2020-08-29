4a219740936c4d4bf47bd5a2f15ed6bb41e62052
Andrew Miracle <me@andrewmiracle.com>
2020-08-29 16:09:45 +0000

    cube server backend

---
+++ index.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const CubejsServerCore = require('@cubejs-backend/server-core');
+
+const app = express();
+app.use(require('cors')());
+app.use(bodyParser.json({ limit: '50mb' }));
+
+const serverCore = CubejsServerCore.create();
+serverCore.initApp(app);
+
+const port = process.env.PORT || 32000;
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Fatal error during server start: ');
+    console.error(e.stack || e);
+  }
+  console.log(`🚀 Cube.js server (${CubejsServerCore.version()}) is listening on ${port}`);
+});
+
+
+
+
+
+
+
+// const CubejsServer = require('@cubejs-backend/server');
+
+// const server = new CubejsServer();
+
+// server.listen().then(({ version, port }) => {
+//   console.log(`🚀 Cube.js server (${version}) is listening on ${port}`);
+// }).catch(e => {
+//   console.error('Fatal error during server start: ');
+//   console.error(e.stack || e);
+// });
